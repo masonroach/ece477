54f6ed3e2b9d84e2650b4a1c46779c3246dba211
patrick <patrick.roy1@maine.edu>
2018-02-04 15:42:50 -0500

    Added printed pyramid functionality. Check lab1.c comments for usage details.

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -14,17 +14,47 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*!
- * @brief A short program to write "Hello World!" to the output terminal.
+ * @brief A short program to write "Hello World!" to the output terminal. If more arguments are used, will print a pyramid of a given string. Usage: "./lab1 [pyramid size] [pyramid string]" or "./lab1 [pyramid size]"
  *
  * @returns	Returns 0 on success, 1 on fail.
  */
 int main(int argc, char *argv[]) {
-	char* output = "Hello World!\n";
+	char* output = "Hello World!";
+	int pyramidSize = 0;
+	char* pyramidString;
+	int i, j, k;
 	
-	if (printf("%s", output) != strlen(output)) {	// Print and then check if somehow something went wrong
+	if (argc == 1 && printf("%s\n", output) != strlen(output) + 1) {	// Print and then check if somehow something went wrong
 		printf("ERROR: printf didn't print enough characters. Should have printed %lu characters.", strlen(output));
+	}
+
+	//If more arguments are present, print pyramid
+	if (argc == 2 || argc == 3) { //If valid number of arguments
+		if (argc == 3) { //If string argument is present
+			pyramidString = argv[2];
+		} else {
+			pyramidString = output;
+		}
+		pyramidSize = atoi(argv[1]); //Second input argument is pyramid size
+		if (pyramidSize < 1000){
+			for (i = 0; i < pyramidSize; i++){ //for each row
+				for (j = 0; j < (pyramidSize - i - 1); j++) { //print spaces before text
+					for (k = 0; k < strlen(pyramidString); k++) { //print spaces of length of string
+						printf(" ");
+					}
+					printf(" "); //print separating space
+				}
+				for (j = 0; j < (2 * i + 1); j++) { //print text and newline
+					printf("%s ", pyramidString);
+				}
+				printf("\n");
+			}
+		} else {
+			printf("Error! Pyramid size too large.\n");
+		}
 	}
 
 	return 0;
