a82799adc579bc54e1d786a6e4d5202e0ef56b5c
patrick <patrick.roy1@maine.edu>
2018-02-09 14:30:25 -0500

    Added new folder for lab 2 with makefile and main code.

---
+++ lab2/lab2.c
@@ -0,0 +1,27 @@
+/*!
+ * @file
+ *
+ * @brief ECE477 Spring 2018 Lab 2 main file
+ *
+ * @author Mason Roach
+ * @author Patrick Roy
+ *
+ * @date Feb 4, 2018
+ *
+ * This program will use the SIGALARM signal to read 7 columns of data from /proc files
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*!
+ * @brief A program to read information from the /proc directory and write to a delimited file to be read by Matlab
+ *
+ * @returns	Returns 0 on success, 1 on fail.
+ */
+int main(int argc, char *argv[]) 
+{
+	return 0;
+} // End main
