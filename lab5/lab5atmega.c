103a3a3fb6bd7883e02451797d2c0be1ef95204c
Patrick John Roy <patrick.roy1@maine.edu>
2018-03-17 15:14:04 -0400

    Completed serial code for ATMega. It compiles, so it works, right?

--- lab5/lab5atmega.c
+++ lab5/lab5atmega.c
@@ -8,26 +8,67 @@
 #include <util/delay.h>
 
 void serialInit(void);
-
+void sendString(char *buffer);
+unsigned char getGuess(void);
 
 int main(void)
 {
+	unsigned char num;
+	unsigned char userGuess = 0;
 	serialInit(); // Initialize serial communication on ATMega88	
-
+	
 	// Infinite loop
 	while (1) {
+		// Wait for correct game start character
+		while(getGuess() != 'S');
+		// Random number between 0-255
+		num = rand() % 256;
+		do {
+			sendString("Enter your guess: ");
+			userGuess = getGuess();
+			if (userGuess < num) sendString("LOW");
+			if (userGuess > num) sendString("HIGH");
+		} while (num != userGuess);
+		sendString("CORRECT! Enter 'S' to play again.");
 	}
+	return 0;
 }
 
 
 void serialInit(void)
 {
-	UBRRH = 0;
+	UBRR0H = 0;
 	// Initializes AVR USART for 4800 baud (assuming 1MHz clock)
 	// 2MHz/(16*(12+1)) = 9615
-	UBRRL = 12; // Set baud rate to 9600 for 2 MHz clock
+	UBRR0L = 12; // Set baud rate to 9600 for 2 MHz clock
 
-	// TODO: Change to 1 stop bit in statement below
-	UCSRC = (1<<URSEL)|(1<<USBS)|(3<<UCSZ0); // 8 bit 2 stop bit no parity
-	UCSRB = (1<<RXEN)|(1<<TXEN); // Enable TX and RX, 8 bit
+	// Lab assignment specifies 1 stop bit, but according to
+	// datasheet, for 8 bit communication 2 stop bits are
+	// REQUIRED
+	UCSR0C = ((1<<USBS0)|(3<<UCSZ00)); // 8 bit 2 stop bit no parity
+	UCSR0C &= ~(3<<UPM00);
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0); // Enable TX and RX, 8 bit
+}
+
+
+
+void sendString(char *buffer)
+{
+	int i;
+	for(i=0; i<strlen(buffer); i++){
+		// Wait for buffer to empty then write new char
+		while((UCSR0A&(1<<UDRE0)) == 0);
+		UDR0 = buffer[i];
+	}
+}
+
+
+unsigned char getGuess(void)
+{
+	unsigned char guess;
+	// Wait for input
+	while(!(UCSR0A&(1<<RXC0)));
+	// return input
+	guess = UDR0;
+	return guess;
 }
