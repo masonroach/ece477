33b1cf18049d2867ccf75b57cf6dbfff6a94070d
Patrick John Roy <patrick.roy1@maine.edu>
2018-03-21 21:11:20 -0400

    Completed Lab 5

--- lab5/lab5atmega.c
+++ lab5/lab5atmega.c
@@ -6,20 +6,38 @@
 
 #define F_CPU 2000000UL
 #include <util/delay.h>
+			 //  A      B        C      D     E     F
+const char* morseDecode[36] = {".-", "-...", "-.-.", "-..", ".", "..-.", 
+			 //   G      H       I     J       K      L
+			    "--.", "....", "..", ".---", "_._", ".-..", 
+			//    M     N     O      P       Q       R      S
+			    "--", "-.", "---", ".--.", "--.-", ".-.", "...",
+			//   T     U      V       W       X      Y       Z
+			    "-", "..-", "...-", ".--", "-..-", "-.--", "--..", 
+			// 0         1       2        3        4        5 
+			"-----", ".----", "..---", "...--", "....-", ".....",
+			// 6        7        8        9
+			"-....", "--...", "---..", "----."};
+
+int morseDelay = 10;
 
 void serialInit(void);
 void sendString(char *buffer);
 unsigned char getGuess(void);
 char getChar(void);
 void putChar(char c);
+void morseInit(int ms);
+void morseMessage(char *msg);
 
 int main(void)
 {
+	char *morseMsg = "Correct";
 	unsigned char num;
 	unsigned char userGuess = 0;
 	serialInit(); // Initialize serial communication on ATMega88	
-	_delay_ms(50);
-	
+	morseInit(30); // Initialize morse code blinker with ms time unit
+	morseMessage("Welcome to the serial high/low game");
+		
 	sendString("Enter 0 to begin.\n\r");	
 	// Infinite loop
 	while (1) {
@@ -33,7 +51,11 @@ int main(void)
 			if (userGuess < num) sendString("LOW\n\r");
 			if (userGuess > num) sendString("HIGH\n\r");
 		} while (num != userGuess);
-		sendString("CORRECT! Enter \"255\" to play again.\n\r");
+		sendString("CORRECT! Enter \"0\" to play again.\n\r");
+
+		// Blink morse message on LED before beginning new game
+		sprintf(morseMsg, "The correct number was %d", num);
+		morseMessage(morseMsg);
 	}
 	return 0;
 }
@@ -71,7 +93,7 @@ unsigned char getGuess(void)
 		// store input
 		guess[i] = getChar();
 		// if user enters a non-numerical character
-		if ((guess[i] < 48) || (guess[i] > 57)) {
+		if ((guess[i] < '0') || (guess[i] > '9')) {
 			// add null terminator and convert to char
 			guess[i] = '\0';
 			break;
@@ -95,4 +117,60 @@ void putChar(char c)
 	// wait for empty transmission buffer
 	while((UCSR0A&(1<<UDRE0)) == 0);
 	UDR0 = c;
+}
+
+void morseInit(int ms)
+{
+	// ms is number of milliseconds for morse unit
+	morseDelay = ms;
+	DDRB |= 0x01; // set PB0 output
+}
+
+void morseMessage(char *msg)
+{
+	// Blinks msg in morse code after win
+	// Valid chars are alphabet and numbers
+	// otherwise, interpret as space
+
+	int i, j, tableIndex = 0;
+	// for each char in string
+	for (i=0;i<strlen(msg);i++){
+		// select char
+		if (msg[i] >= 'a' && msg[i] <= 'z') {
+			// lowercase letters
+			tableIndex = msg[i] - 'a';
+		} else if (msg[i] >= 'A' && msg[i] <= 'Z') {
+			// uppercase letters
+			tableIndex = msg[i] - 'A';
+		} else if (msg[i] >= '0' && msg[i] <= '9') {
+			// numbers come after spaces in lookup table
+			tableIndex = msg[i] - '0' + 26;
+		} else {
+			tableIndex = -1; // print space
+		}
+		
+		// blink char
+		for (j=0; tableIndex>0 && j<strlen(morseDecode[tableIndex]); j++){
+			if (morseDecode[tableIndex][j] == '.'){
+				PORTB |= 0x01; // LED on
+				_delay_ms(morseDelay); // dot, 1 time unit
+				PORTB &= ~(0x01); // LED off
+				_delay_ms(morseDelay);
+			} else {
+				PORTB |= 0x01; // LED on
+                                _delay_ms(3 * morseDelay); // dash, 3 time units
+                                PORTB &= ~(0x01); // LED off
+                                _delay_ms(morseDelay);
+			}
+		}
+
+		// if space or invalid char, blink space
+		if (tableIndex == -1) {
+			_delay_ms(4*morseDelay); // low 7 time units
+			// Only 4 to account for 3 below
+		}
+
+		// space between letters is 3 time units
+		_delay_ms(3*morseDelay);
+	}
 }
--- lab5/lab5pc.c
+++ lab5/lab5pc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #define FILENAME "/dev/ACM0"
 
@@ -7,24 +8,60 @@ int main(int argc, char **argv)
 {
 	// This program will read and write serial 
 	// to talk to atmega88 to play high/low game
-	FILE* sfile;
-	int c = 0;
+	FILE *sfile;
+	char getStr[50];
+	unsigned char guess;
 
 	// Open device file and error check
 	sfile = fopen(FILENAME, "r+");
 	if (sfile == NULL) {
-		printf("ERROR: Unable to open device file\n");
+		printf("ERROR: Unable to open device file, code %d\n", errno);
 		exit(0);
 	}
 
 	
 	while (1) {
+		
+		// Read and print start of game text
+		if (fgets(getStr, 50, sfile) == NULL) {
+			printf("ERROR: Unable to read from device file 1, code %d\n", errno);
+			exit(0);
+		}		
+		printf("%s\n", getStr);	
+	
+		guess = 127;
+		
 		// Game start character
-		fputs("0", sfile);
+		fputc('0', sfile);
+
+		// For each guessing round
+		while(1){
+			// Receives "Enter your guess:"
+			if (fgets(getStr, 1, sfile) == NULL) {
+				printf("ERROR: Unable to read from device file 2, code %d\n", errno);
+                        	exit(0);
+			}
+			printf("%s\n", getStr);
+			
+			// Makes guess
+			sprintf(getStr, "%d", guess);
+			fputs(getStr, sfile);
 		
-		// Read start of game text
-		while ((c == getc(sfile)) != EOF) printf("%c", c);
-		
+			// Receives feedback, "HIGH", "LOW", or "CORRECT"
+			if (fgets(getStr, 1, sfile) == NULL) {
+                                printf("ERROR: Unable to read from device file 3, code %d\n", errno);
+                                exit(0);
+                        }
+			printf("%s\n", getStr);
+
+			// Adjust guess or quit game with communication error
+			if (getStr[0] == 'L') guess /= 2; // low
+			else if (getStr[0] == 'H') guess += (255 - guess) / 2; // high
+			else if (getStr[0] == 'C') { printf("Correct number: %d\n", guess); break; } // correct
+			else { printf("Incorrect feedback, received %c, exiting\n", getStr[0]); exit(0); }
+			
+			
+		}
 	}
 	
 	fclose(sfile);
